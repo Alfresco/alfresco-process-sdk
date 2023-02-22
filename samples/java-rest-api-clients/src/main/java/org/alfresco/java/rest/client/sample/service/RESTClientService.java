ef6c05aa1d0ee39e6ded9d5b4799f1fe6ed94a77
Ryan Wilson <84201908+ryewilson@users.noreply.github.com>
2023-02-22 08:26:22 -0500

    Fix code formatting reported by Codacy (#8)

--- samples/java-rest-api-clients/src/main/java/org/alfresco/java/rest/client/sample/service/RESTClientService.java
+++ samples/java-rest-api-clients/src/main/java/org/alfresco/java/rest/client/sample/service/RESTClientService.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.alfresco.java.rest.client.sample.service;
 
 import org.alfresco.activiti.audit.handler.AuditEventsControllerImplApi;
